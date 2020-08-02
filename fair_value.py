34aa985c75107c68afa0a0b3f25e094ce092a630
Wilhelm Uggla <wille@gabrieluggla.se>
2020-08-02 20:33:53 +0200

    update

--- fair_value.py
+++ fair_value.py
@@ -7,17 +7,18 @@ import os
 import sys
 from colorama import Fore, Style
 
+#read portfolio value from portfolio.json
 with open(os.path.join(sys.path[0], 'portfolio.json'), "r") as f:
   portfolio = json.load(f)
 
-
+#sets date
 now = datetime.datetime.now()
 
+#overall portfolio value
 portfolio_value = 0
 
 
 print(f"\n\n{Fore.RED}Fair value calculation - " + now.strftime("%Y-%m-%d %H:%M") +  f" {Style.RESET_ALL}\n")
-
 
 for crypto in portfolio:
     #getting the page
@@ -37,10 +38,13 @@ for crypto in portfolio:
 
     value = price * crypto['amount']
 
+    #updating portfolio value
     portfolio_value += value  
 
     print(crypto['crypto'] + " " + str(crypto['amount']) + " * " + str(price) + " = " + str(round(value, 2)) + "kr")
 
+#rounds portfolio value
 portfolio_value = round(portfolio_value, 2)
 
+#print portfolio value
 print(f"\nPortfolio Value: \033[4m" + str(portfolio_value) + f"\033[0mkr\n\n")
